6ec267c590186c31f36adbb8682113b875a06354
modimore <quinn.e.mortimer@gmail.com>
2017-03-23 16:14:22 -0400

    Consistently use tabs for spacing wherever possible.
    Previously both two and four spaces where in use for spacing in
    various places.
    Both of those styles have been replaced with tabs where possible.
    
    The change affects files with these extensions:
    .html
    .scss
    .svg
    and does not affect files with this extension:
    .yml
    
    No files with extension .md were affected by the change.

--- js/site.js
+++ js/site.js
@@ -3,19 +3,19 @@
 nav_toggle = document.getElementById('nav-toggle');
 
 function toggle_nav_links(event) {
-  var nav_link_lists = document.getElementsByClassName('nav-links');
+	var nav_link_lists = document.getElementsByClassName('nav-links');
 
-  if (event.target == nav_toggle || nav_toggle.parentElement.contains(event.target)) {
-    for (var i = 0; i < nav_link_lists.length; i++) {
-      nav_link_lists[i].classList.add("expanded");
-    }
-  }
-  else {
-    for (var i = 0; i < nav_link_lists.length; i++) {
-      nav_link_lists[i].classList.remove("expanded");
-    }
-  }
-  event.stopPropagation();
+	if (event.target == nav_toggle || nav_toggle.parentElement.contains(event.target)) {
+		for (var i = 0; i < nav_link_lists.length; i++) {
+			nav_link_lists[i].classList.add("expanded");
+		}
+	}
+	else {
+		for (var i = 0; i < nav_link_lists.length; i++) {
+			nav_link_lists[i].classList.remove("expanded");
+		}
+	}
+	event.stopPropagation();
 }
 
 document.addEventListener('click', toggle_nav_links);
